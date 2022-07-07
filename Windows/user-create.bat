fbcd74a177bacec656a321ca08bf72339d4ddf2f
pinksheer <2739927391@qq.com>
2022-07-07 23:45:19 +0800

    init

---
+++ Windows/user-create.bat
@@ -0,0 +1,11 @@
+@echo off
+net config server /srvcomment:"Windows Azure VM" >nul
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F >nul
+net user Admin 123456 /add >nul
+net localgroup administrators Admin /add >nul
+net user Admin /active:yes >nul
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant Admin:F >nul
+ICACLS C:\Windows\installer /grant Admin:F >nul
