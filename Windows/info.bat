44957db031f4419356b32f291bc03ff59492168f
pinksheer <78209749+pinksheer@users.noreply.github.com>
2022-07-08 00:05:25 +0800

    userinfo

--- Windows/info.bat
+++ Windows/info.bat
@@ -3,7 +3,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels"
 echo.
-echo User: Administrator
-echo Pass: Pass@!1270
+echo User: Admin
+echo Pass: 1234@Pass
 echo.
 echo Maximum VM Time: 6 hours
