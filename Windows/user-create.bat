ea41115d4213605c7682550f5872ccbb11e5d082
pinksheer <78209749+pinksheer@users.noreply.github.com>
2022-07-08 00:00:46 +0800

    password

--- Windows/user-create.bat
+++ Windows/user-create.bat
@@ -1,7 +1,7 @@
 @echo off
 net config server /srvcomment:"Windows Azure VM" >nul
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F >nul
-net user Admin 123456 /add >nul
+net user Admin 1234@Pass /add >nul
 net localgroup administrators Admin /add >nul
 net user Admin /active:yes >nul
 diskperf -Y >nul
